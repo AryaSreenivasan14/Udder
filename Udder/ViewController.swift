54a73d20374b613277f1904202d2aecf7d46559c
Arya Sreenivasan <aryasreenivasan14@gmail.com>
2018-09-25 16:42:25 +1000

    new commit

--- Udder/ViewController.swift
+++ Udder/ViewController.swift
@@ -13,6 +13,11 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         print("hi")
+        print("hi")
+        print("hi")
+        print("hi")
+        print("hi")
+
         // Do any additional setup after loading the view, typically from a nib.
     }
 
